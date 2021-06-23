b7b6c044457ee59239db80cc579d7023afbebb18
toterGott <alex7lf@gmail.com>
2021-06-24 01:35:41 +0300

    Added Telegram bot library and simple message handling

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Telegram article storage bot
 
-TODO:
+## Usage
+Set token API in `TELOXIDE_TOKEN` variable
+
+## TODO:
 - [ ] Add Telegram bot API support
 - [ ] Configure CI
 - [ ] Add ability to store article links
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting dices_bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot, |message| async move {
+        log::info!("id: {}", message.update.id);
+        log::info!("date: {}", message.update.date);
+        log::info!("update.chat.id: {}", message.update.chat.id);
+
+        message.answer_dice().await?;
+
+        match message.update.text().map(ToOwned::to_owned) {
+            None => {
+                message.answer("Send me a text message.").await?;
+            }
+            Some(ans) => {
+                log::info!("User's message: {}", ans);
+                let mut response_message: String = "Message received: ".to_owned();
+                response_message.push_str(&ans);
+                message.answer(response_message).await?;
+            }
+        }
+
+        respond(())
+    })
+        .await;
 }
