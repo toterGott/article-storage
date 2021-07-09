8356a68fb72e94b2146258ad130a0b641c2287b6
toterGott <alex7lf@gmail.com>
2021-07-09 20:06:17 +0300

    Trying to select from DB

--- src/db_manager.rs
+++ src/db_manager.rs
@@ -1,14 +1,28 @@
 use sqlite;
 use sqlite::{Connection};
+use sqlite::State;
 
 pub async fn save_link(message: &str, user_id: i64) {
     save_user(user_id).await;
-    execute_query(&format!("INSERT INTO article_link (link) VALUES ('{}');", message)).await;
+    execute_query(&format!("INSERT OR IGNORE INTO article_link (link) VALUES ('{}');", message)).await;
+    let connection = get_connection();
+
+    let mut statement = connection
+        .prepare(&format!("SELECT  id FROM article_link WHERE link =  '{}'", message))
+        .unwrap();
+    match statement.bind_by_name("id", int::class) {
+        Ok(x) => x,
+        Err(error) => log::info!("ERROR: {}", error),
+    };
+    while let State::Row = statement.next().unwrap() {
+        println!("id = {}", statement.read::<String>(0).unwrap());
+    }
+
     log::info!("Link saved");
 }
 
 async fn save_user(user_id: i64) {
-    execute_query(&format!("INSERT INTO bot_user (user_id) VALUES ('{}')", user_id)).await;
+    execute_query(&format!("INSERT OR IGNORE INTO bot_user (user_id) VALUES ('{}')", user_id)).await;
 }
 
 
